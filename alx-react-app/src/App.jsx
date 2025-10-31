19d625210247572e06d4152effb81ff8ff2be80d
Sophie Akoth <akothsophie9023.gmail.com>
2025-10-31 13:13:36 +0300

    Corrections on App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,20 +1,38 @@
 
-import Header from './components/Header.jsx'
-import MainContent from './components/MainContent.jsx'
-import Footer from './components/Footer.jsx'
+// import Header from './components/Header.jsx'
+// import MainContent from './components/MainContent.jsx'
+// import Footer from './components/Footer.jsx'
 
 
-function App() {
+// function App() {
   
 
+//   return (
+//     <>
+//        <Header/>
+//        <MainContent/>
+//        <Footer/>
+       
+//     </>
+//   )
+// }
+
+// export default App
+
+
+
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+
+function App() {
   return (
     <>
-       <Header/>
-       <MainContent/>
-       <Footer/>
-       
+      <Header />
+      <MainContent />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
