228cf16afe1f4ad6cdb4e552ddb9aa2ef5d30740
Sophie Akoth <akothsophie9023.gmail.com>
2025-10-31 12:59:04 +0300

    rendered Header,mainContent and Footer

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -12,10 +12,10 @@ function App() {
   return (
     <>
         <Header/>
-        <WelcomeMessage /> 
+        {/* <WelcomeMessage />  */}
        <MainContent/>
       
-       <UserProfile   name="Alice" age="25" bio="Loves hiking and photography"/>
+       {/* <UserProfile   name="Alice" age="25" bio="Loves hiking and photography"/> */}
        <Footer/>
       
     </>
