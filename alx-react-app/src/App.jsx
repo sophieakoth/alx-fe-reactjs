9bae0553684f6430cf9e3ec4faea6bb06c5613c3
Sophie Akoth <akothsophie9023.gmail.com>
2025-10-31 13:05:03 +0300

    return correction on app.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,9 +1,7 @@
 
 import Header from './components/Header.jsx'
-import WelcomeMessage from './components/WelcomeMessage.jsx'
 import MainContent from './components/MainContent.jsx'
 import Footer from './components/Footer.jsx'
-import UserProfile from './components/UserProfile.jsx'
 
 
 function App() {
@@ -12,12 +10,8 @@ function App() {
   return (
     <>
         <Header/>
-        {/* <WelcomeMessage />  */}
        <MainContent/>
-      
-       {/* <UserProfile   name="Alice" age="25" bio="Loves hiking and photography"/> */}
        <Footer/>
-      
     </>
   )
 }
