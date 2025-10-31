9e9197c488747128b6882b0858849f594c73f89b
Sophie Akoth <akothsophie9023.gmail.com>
2025-10-31 13:18:02 +0300

    Rendered userprofile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,35 +1,20 @@
 
-// import Header from './components/Header.jsx'
-// import MainContent from './components/MainContent.jsx'
-// import Footer from './components/Footer.jsx'
 
 
-// function App() {
-  
-
-//   return (
-//     <>
-//        <Header/>
-//        <MainContent/>
-//        <Footer/>
-       
-//     </>
-//   )
-// }
-
-// export default App
 
 
 
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile.jsx';
 
 function App() {
   return (
     <>
       <Header />
       <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Footer />
     </>
   );
