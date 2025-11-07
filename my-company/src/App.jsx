b8938e3ee2a65be268bd1917faabede8a652ae50
Sophie Akoth <akothsophie9023.gmail.com>
2025-11-07 18:55:14 +0300

    Navbar corrections

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,33 +1,22 @@
- import { BrowserRouter,Routes,Route } from "react-router-dom"
- import Home from './components/Home.jsx'
- import Navbar from "./components/Navbar.jsx"
- import About from './components/About.jsx'
- import Contact from "./components/Contact.jsx"
- import Services from "./components/Services.jsx"
-
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from './components/Home.jsx'
+import Navbar from "./components/Navbar.jsx"
+import About from './components/About.jsx'
+import Contact from "./components/Contact.jsx"
+import Services from "./components/Services.jsx"
 
 function App() {
-  
-
   return (
-  <BrowserRouter>
-  <Navbar/>
-  
-   <Routes>
-   <Route path='/' element = {<Home/>}/>
-   <Route path='/about' element = {<About/>}/>
-   <Route path='/contact' element = {<Contact/>}/>
-   <Route path='/services' element = {<Services/>}/>
-  
-  
-   </Routes>
-  
-   </BrowserRouter>
+    <BrowserRouter>
+      <Navbar/>
 
-    
-     
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/services' element={<Services/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- my-company/src/Components/Navbar.jsx
+++ my-company/src/Components/Navbar.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 
-function Navbar(){
-    return(<>
-    <nav style={{ padding: "10px", background: "#eee" }}>
-    <Link to ="/" style={{ margin: "0 10px" }}>Home</Link>
-    <Link to ="/about" style={{ margin: "0 10px" }}>About</Link>
-    <Link to ="/contact" style={{ margin: "0 10px" }}>Contact</Link>
-    <Link to ="/services" style={{ margin: "0 10px" }}>Services</Link>
-  </nav>
-    </>);
+function Navbar() {
+  return (
+    <>
+      <nav style={{ padding: "10px", background: "#eee" }}>
+        <Link to="/" style={{ margin: "0 10px" }}>Home</Link>
+        <Link to="/about" style={{ margin: "0 10px" }}>About</Link>
+        <Link to="/contact" style={{ margin: "0 10px" }}>Contact</Link>
+        <Link to="/services" style={{ margin: "0 10px" }}>Services</Link>
+      </nav>
+    </>
+  );
 }
+
 export default Navbar
