edfd9ae9f1b340417d9c463a161507d6c25ffef0
Sophie Akoth <akothsophie9023.gmail.com>
2025-11-07 10:38:49 +0300

    Added Counter.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -8,11 +8,13 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile.jsx';
+import Counter from './components/Counter.jsx';
 
 function App() {
   return (
     <>
       <Header />
+      <Counter/>
       <MainContent />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Footer />
