ca9173067e641c2a78b70ef6ed37aff41f39063c
Sophie Akoth <akothsophie9023.gmail.com>
2025-11-09 21:37:01 +0300

    Corrections on Navbar.jsx

--- my-company/src/Components/Navbar.jsx
+++ my-company/src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 
 function Navbar() {
   return (
-    <>
+    
       <nav style={{ padding: "10px", background: "#eee" }}>
         <Link to="/" style={{ margin: "0 10px" }}>Home</Link>
         <Link to="/about" style={{ margin: "0 10px" }}>About</Link>
@@ -12,8 +12,8 @@ function Navbar() {
       </nav>
       
 
-    </>
+    
   );
 }
 
-export default Navbar
+export default Navbar;
