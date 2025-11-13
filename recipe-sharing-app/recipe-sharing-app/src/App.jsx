25b4b1e5f7bb8712aa5cfd53d1861b9dc6b881ce
Sophie Akoth <akothsophie9023.gmail.com>
2025-11-13 10:41:13 +0300

    Added the basic files

---
+++ recipe-sharing-app/recipe-sharing-app/src/App.jsx
@@ -0,0 +1,18 @@
+import RecipeList from './components/RecipeList'
+import  AddRecipeForm from './components/AddRecipeForm'
+
+
+function App() {
+
+
+  return (
+  <>
+<AddRecipeForm/>
+  <RecipeList/>
+  </>
+    
+    
+  )
+}
+
+export default App
