aaff3874cff4fcb1d66c7031405c6e4a5b14e944
Sophie Akoth <akothsophie9023.gmail.com>
2025-11-13 16:18:07 +0300

    RecipeList.jsx  correction

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,5 +1,7 @@
 import { useRecipeStore } from './recipeStore.js';
 import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
 
 
   const RecipeList = () => {
@@ -21,7 +23,10 @@ import { useEffect } from 'react';
       <div>
         {recipes.map(recipe => (
           <div key={recipe.id}>
-            <h3>{recipe.title}</h3>
+            <h3>
+            <Link to={`/recipes/${recipe.id}`}>{recipe.title}</Link>
+
+            </h3>
             <p>{recipe.description}</p>
           </div>
         ))}
