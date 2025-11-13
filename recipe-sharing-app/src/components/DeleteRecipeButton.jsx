0c58eef3c4ddba0dcb2b3f3b6fc86903c96cb0c2
Sophie Akoth <akothsophie9023.gmail.com>
2025-11-13 15:37:43 +0300

    DeleteRecipeButton navigate corrections

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,14 +1,17 @@
-// DeleteRecipeButton.jsx
+
 import React from 'react';
 import { useRecipeStore } from './recipeStore';
+import { useNavigate } from 'react-router-dom';
 
 const DeleteRecipeButton = ({ recipeId }) => {
   const deleteRecipe = useRecipeStore(state => state.deleteRecipe);
+  const navigate = useNavigate(); 
 
   const handleDelete = () => {
     const confirmed = window.confirm('Are you sure you want to delete this recipe?');
     if (confirmed) {
       deleteRecipe(recipeId);
+      navigate('/recipes'); 
     }
   };
 
