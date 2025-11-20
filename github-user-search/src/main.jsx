acc4de8356f5d12894405a7814feeb5bc86cb33f
Sophie Akoth <akothsophie9023.gmail.com>
2025-11-20 18:50:42 +0300

    tailwindcss work

--- github-user-search/src/main.jsx
+++ github-user-search/src/main.jsx
@@ -4,6 +4,7 @@ import './index.css';
 
 
 
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
---
+++ github-user-search/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    ".index.html",
+    "./src/components/Footer.jsx",
+    "./src/components/Header.jsx",
+    "./src/components/MainContent.jsx",
+    "./src/services/githubServices.jsx"
+    
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
